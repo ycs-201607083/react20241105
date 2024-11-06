00d56c9ec91a223e00f490e0703da3fc93033d0e
ycs-201607083 <nan0805@naver.com>
2024-11-06 16:36:48 +0900

    ㅇㅇ

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,13 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 // import MyApps from "./apps/MyApp24.jsx";
 import MyApps from "./practice/App24.jsx";
+import {Provider} from "./components/ui/provider.jsx"
+
 createRoot(document.getElementById("root")).render(
-  <StrictMode>
-      <MyApps/>
+    <StrictMode>
+        <Provider>
+            <MyApps/>
+        </Provider>
+
   </StrictMode>
 );
